e8f253bc56fdcac13ac0d11638886005eddddc43
slate63 <StrodaJM@gmail.com>
2020-06-02 19:41:01 -0400

    Changing it up. Adding progress bar instead of adding gadges

--- docker/standalone.php
+++ docker/standalone.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no" />
-<title>LibreSpeed Example</title>
+<meta charset="UTF-8" />
 <link rel="shortcut icon" href="favicon.ico">
 <script type="text/javascript" src="speedtest.js"></script>
 <script type="text/javascript">
 function I(i){return document.getElementById(i);}
 //INITIALIZE SPEEDTEST
 var s=new Speedtest(); //create speedtest object
+<?php if(getenv("TELEMETRY")=="true"){ ?>
+s.setParameter("telemetry_level","basic");
+<?php } ?>
+<?php if(getenv("DISABLE_IPINFO")=="true"){ ?>
+s.setParameter("getIp_ispInfo","false");
+<?php } ?>
+<?php if(getenv("DISTANCE")){ ?>
+s.setParameter("getIp_ispInfo_distance","<?=getenv("DISTANCE") ?>");
+<?php } ?>
 
 var meterBk=/Trident.*rv:(\d+\.\d+)/i.test(navigator.userAgent)?"#EAEAEA":"#80808040";
 var dlColor="#6060AA",
@@ -65,12 +73,26 @@ function startStop(){
 	}else{
 		//test is not running, begin
 		I("startStopBtn").className="running";
+		I("shareArea").style.display="none";
 		s.onupdate=function(data){
             uiData=data;
 		};
 		s.onend=function(aborted){
             I("startStopBtn").className="";
             updateUI(true);
+            if(!aborted){
+                //if testId is present, show sharing panel, otherwise do nothing
+                try{
+                    var testId=uiData.testId;
+                    if(testId!=null){
+                        var shareURL=window.location.href.substring(0,window.location.href.lastIndexOf("/"))+"/results/?id="+testId;
+                        I("resultsImg").src=shareURL;
+                        I("resultsURL").value=shareURL;
+                        I("testId").innerHTML=testId;
+                        I("shareArea").style.display="";
+                    }
+                }catch(e){}
+            }
 		};
 		s.start();
 	}
@@ -87,8 +109,6 @@ function updateUI(forced){
 	drawMeter(I("ulMeter"),mbpsToAmount(Number(uiData.ulStatus*(status==3?oscillate():1))),meterBk,ulColor,Number(uiData.ulProgress),progColor);
 	I("pingText").textContent=format(uiData.pingStatus);
 	I("jitText").textContent=format(uiData.jitterStatus);
-	var prog=(Number(uiData.dlProgress)*2+Number(uiData.ulProgress)*2+Number(uiData.pingProgress))/5;
-    I("progress").style.width=(100*prog)+"%";
 }
 function oscillate(){
 	return 1+0.02*Math.sin(Date.now()/100);
@@ -111,7 +131,6 @@ function initUI(){
 	I("ip").textContent="";
 }
 </script>
-
 <style type="text/css">
 	html,body{
 		border:none; padding:0; margin:0;
@@ -164,113 +183,183 @@ function initUI(){
 		position:relative;
 		box-sizing:border-box;
 	}
-	div.testName{
+	div.testArea2{
+		display:inline-block;
+		width:14em;
+		height:7em;
+		position:relative;
+		box-sizing:border-box;
+		text-align:center;
+	}
+	div.testArea div.testName{
 		position:absolute;
 		top:0.1em; left:0;
 		width:100%;
 		font-size:1.4em;
 		z-index:9;
 	}
-	div.meterText{
+	div.testArea2 div.testName{
+        display:block;
+        text-align:center;
+        font-size:1.4em;
+	}
+	div.testArea div.meterText{
 		position:absolute;
-		bottom:1.5em; left:0;
+		bottom:1.55em; left:0;
 		width:100%;
 		font-size:2.5em;
 		z-index:9;
 	}
-	#dlText{
-		color:#6060AA;
-	}
-	#ulText{
-		color:#309030;
-	}
-	#pingText,#jitText{
-		color:#AA6060;
+	div.testArea2 div.meterText{
+        display:inline-block;
+        font-size:2.5em;
 	}
 	div.meterText:empty:before{
-		color:#505050 !important;
 		content:"0.00";
 	}
-	div.unit{
+	div.testArea div.unit{
 		position:absolute;
 		bottom:2em; left:0;
 		width:100%;
 		z-index:9;
 	}
-	div.testGroup{
+	div.testArea2 div.unit{
 		display:inline-block;
 	}
-	@media all and (max-width:65em){
-		body{
-			font-size:1.5vw;
-		}
+	div.testArea canvas{
+		position:absolute;
+		top:0; left:0; width:100%; height:100%;
+		z-index:1;
+	}
+	div.testGroup{
+		display:block;
+        margin: 0 auto;
+	}
+	#shareArea{
+		width:95%;
+		max-width:40em;
+		margin:0 auto;
+		margin-top:2em;
+	}
+	#shareArea > *{
+		display:block;
+		width:100%;
+		height:auto;
+		margin: 0.25em 0;
+	}
+	#privacyPolicy{
+        position:fixed;
+        top:2em;
+        bottom:2em;
+        left:2em;
+        right:2em;
+        overflow-y:auto;
+        width:auto;
+        height:auto;
+        box-shadow:0 0 3em 1em #000000;
+        z-index:999999;
+        text-align:left;
+        background-color:#FFFFFF;
+        padding:1em;
+	}
+	a.privacy{
+        text-align:center;
+        font-size:0.8em;
+        color:#808080;
+        display:block;
 	}
 	@media all and (max-width:40em){
 		body{
 			font-size:0.8em;
 		}
-		div.testGroup{
-			display:block;
-			margin: 0 auto;
-		}
 	}
-	#progressBar{
-		width:90%;
-		height:0.3em;
-		background-color:#EEEEEE;
-		position:relative;
-		display:block;
-		margin:0 auto;
-		margin-bottom:2em;
-	}
-	#progress{
-		position:absolute;
-		top:0; left:0;
-		height:100%;
-		width:0%;
-		transition: width 2s;
-		background-color:#90BBFF;
-	}
-
 </style>
+<title><?= getenv('TITLE') ?: 'LibreSpeed Example' ?></title>
 </head>
 <body>
-<h1>Speedtest Internal</h1>
-<div id="startStopBtn" onclick="startStop()"></div>
-<div id="test">
-    <div id="progressBar"><div id="progress"></div></div>
-	<div class="testGroup">
-		<div class="testArea">
-			<div class="testName">Download</div>
-			<canvas id="dlMeter" class="meter"></canvas>
-			<div id="dlText" class="meterText"></div>
-			<div class="unit">Mbps</div>
+<h1><?= getenv('TITLE') ?: 'LibreSpeed Example' ?></h1>
+<div id="testWrapper">
+	<div id="startStopBtn" onclick="startStop()"></div><br/>
+	<?php if(getenv("TELEMETRY")=="true"){ ?>
+        <a class="privacy" href="#" onclick="I('privacyPolicy').style.display=''">Privacy</a>
+	<?php } ?>
+	<div id="test">
+		<div class="testGroup">
+			<div class="testArea2">
+				<div class="testName">Ping</div>
+				<div id="pingText" class="meterText" style="color:#AA6060"></div>
+				<div class="unit">ms</div>
+			</div>
+			<div class="testArea2">
+				<div class="testName">Jitter</div>
+				<div id="jitText" class="meterText" style="color:#AA6060"></div>
+				<div class="unit">ms</div>
+			</div>
 		</div>
-		<div class="testArea">
-			<div class="testName">Upload</div>
-			<canvas id="ulMeter" class="meter"></canvas>
-			<div id="ulText" class="meterText"></div>
-			<div class="unit">Mbps</div>
+		<div class="testGroup">
+			<div class="testArea">
+				<div class="testName">Download</div>
+				<canvas id="dlMeter" class="meter"></canvas>
+				<div id="dlText" class="meterText"></div>
+				<div class="unit">Mbps</div>
+			</div>
+			<div class="testArea">
+				<div class="testName">Upload</div>
+				<canvas id="ulMeter" class="meter"></canvas>
+				<div id="ulText" class="meterText"></div>
+				<div class="unit">Mbps</div>
+			</div>
+		</div>
+		<div id="ipArea">
+			<span id="ip"></span>
+		</div>
+		<div id="shareArea" style="display:none">
+			<h3>Share results</h3>
+			<p>Test ID: <span id="testId"></span></p>
+			<input type="text" value="" id="resultsURL" readonly="readonly" onclick="this.select();this.focus();this.select();document.execCommand('copy');alert('Link copied')"/>
+			<img src="" id="resultsImg" />
 		</div>
 	</div>
-	<div class="testGroup">
-		<div class="testArea">
-			<div class="testName">Ping</div>
-			<div id="pingText" class="meterText"></div>
-			<div class="unit">ms</div>
-		</div>
-		<div class="testArea">
-			<div class="testName">Jitter</div>
-			<div id="jitText" class="meterText"></div>
-			<div class="unit">ms</div>
-		</div>
-	</div>
-	<div id="ipArea">
-		IP Address: <span id="ip"></span>
-	</div>
+	<a href="https://github.com/librespeed/speedtest">Source code</a>
 </div>
-<script type="text/javascript">
-    initUI();
-</script>
+<div id="privacyPolicy" style="display:none">
+    <h2>Privacy Policy</h2>
+    <p>This HTML5 Speedtest server is configured with telemetry enabled.</p>
+    <h4>What data we collect</h4>
+    <p>
+        At the end of the test, the following data is collected and stored:
+        <ul>
+            <li>Test ID</li>
+            <li>Time of testing</li>
+            <li>Test results (download and upload speed, ping and jitter)</li>
+            <li>IP address</li>
+            <li>ISP information</li>
+            <li>Approximate location (inferred from IP address, not GPS)</li>
+            <li>User agent and browser locale</li>
+            <li>Test log (contains no personal information)</li>
+        </ul>
+    </p>
+    <h4>How we use the data</h4>
+    <p>
+        Data collected through this service is used to:
+        <ul>
+            <li>Allow sharing of test results (sharable image for forums, etc.)</li>
+            <li>To improve the service offered to you (for instance, to detect problems on our side)</li>
+        </ul>
+        No personal information is disclosed to third parties.
+    </p>
+    <h4>Your consent</h4>
+    <p>
+        By starting the test, you consent to the terms of this privacy policy.
+    </p>
+    <h4>Data removal</h4>
+    <p>
+        If you want to have your information deleted, you need to provide either the ID of the test or your IP address. This is the only way to identify your data, without this information we won't be able to comply with your request.<br/><br/>
+        Contact this email address for all deletion requests: <a href="mailto:<?=getenv("EMAIL") ?>"><?=getenv("EMAIL") ?></a>.
+    </p>
+    <br/><br/>
+    <a class="privacy" href="#" onclick="I('privacyPolicy').style.display='none'">Close</a><br/>
+</div>
+<script type="text/javascript">setTimeout(function(){initUI()},100);</script>
 </body>
 </html>
