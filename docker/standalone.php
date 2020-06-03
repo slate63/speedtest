81ce3b2d3fb0488ca460a3313ff4a228ef76af62
slate63 <StrodaJM@gmail.com>
2020-06-02 20:51:18 -0400

    Update standalone.php

--- docker/standalone.php
+++ docker/standalone.php
@@ -224,7 +224,9 @@ function initUI(){
 		z-index:9;
 	}
 	div.testArea2 div.unit{
-		display:inline-block;
+		position:absolute;
+		top:0; left:0; width:100%; height:100%;
+		z-index:1;
 	}
 	div.testArea canvas{
 		position:absolute;
